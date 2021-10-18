07cf8536631eaa4b1ae70a2ab0c6c154a1e3b486
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2021-10-18 16:46:47 +0300

    Generated React build for heroku

---
+++ server/build/precache-manifest.bc54268eb4a00a0c24eb1914f4a5988e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d800325f0d9176b5662b60a9a313854",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8147af796d432dbef569",
+    "url": "./static/css/main.ae18e75f.chunk.css"
+  },
+  {
+    "revision": "4653ede4603a72836626",
+    "url": "./static/js/2.776b7cc9.chunk.js"
+  },
+  {
+    "revision": "571f7b210fa45309594f1aa7a8df7621",
+    "url": "./static/js/2.776b7cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8147af796d432dbef569",
+    "url": "./static/js/main.8a787da6.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
